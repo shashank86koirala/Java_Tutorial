fbc9a7ea56f741a24a4c61e91d566315bc61aed2
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-21 22:06:38 +0545

    push

---
+++ DynamicArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class DynamicArray {
+    public static void main(String[] args) {
+        int size, i;
+        Scanner ref = new Scanner(System.in);
+        size = ref.nextInt();
+        System.out.println("Enter to your desire array");
+        int a[] = new int[size];
+        for (i = 0; i < size; i++) {
+            a[i] = ref.nextInt();
+
+        }
+        System.out.print("Printed Array");
+        for (i = 0; i < size; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
