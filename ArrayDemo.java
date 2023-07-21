7968e619622e087a89e10280fbb4b4749e458271
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-21 21:48:09 +0545

    pull

---
+++ ArrayDemo.java
@@ -0,0 +1,6 @@
+class ArrayDemo {
+    public static void main(String[] args) {
+        int a[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+        System.out.println(a[4]);
+    }
+}
---
+++ StaticArray.java
@@ -0,0 +1,14 @@
+class StaticArray {
+    public static void main(String[] args) {
+        int a[] = new int[6];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 40;
+        a[3] = 50;
+        a[4] = 30;
+        a[5] = 60;
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
