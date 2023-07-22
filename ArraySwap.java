a1ce938e4375ca0b40a5914c39efdbb6011e06a3
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-22 15:07:36 +0545

    push

---
+++ ArraySwap.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class ArraySwap {
+    public static void main(String[] args) {
+        int temp, sum = 0;
+        Scanner ref = new Scanner(System.in);
+        int a[] = new int[6];
+        System.out.print("enter data in array");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = ref.nextInt();
+        }
+        System.out.print("Displaying data of array");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+
+            }
+
+        }
+        System.out.print("Displaying result after arranging the array");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
+    }
+}
