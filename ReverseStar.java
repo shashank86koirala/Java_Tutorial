923b199e194be58c1eb34a4e1ebc76a6f877fd7f
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-26 22:48:42 +0545

    push

---
+++ ReverseStar.java
@@ -0,0 +1,12 @@
+class ReverseStar {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 5; i >= 1; i--) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
