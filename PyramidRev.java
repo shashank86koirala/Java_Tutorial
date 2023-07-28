614a06c30103687d088a473387f24820c6de4ed3
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-28 22:35:54 +0545

    push

---
+++ PyramidRev.java
@@ -0,0 +1,15 @@
+class PyramidRev {
+    public static void main(String[] args) {
+        int i, j, k;
+        for (i = 1; i <= 5; i++) {
+            for (k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (j = i; j <= 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
