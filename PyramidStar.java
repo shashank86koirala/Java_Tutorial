754dd17273bd393728361fdc97ebbf31864c6e01
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-28 22:18:30 +0545

    push

---
+++ PyramidStar.java
@@ -0,0 +1,17 @@
+class PyramidStar {
+    public static void main(String[] args) {
+        int i, j, k;
+        for (i = 1; i <= 5; i++) {
+            for (j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= i; k++) {
+                System.out.print("*");
+
+            }
+            System.out.print("\n");
+
+        }
+
+    }
+}
