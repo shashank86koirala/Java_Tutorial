319ef8c3736e7cd3752f9925f101b891e3e8d6e1
shashank86koirala <shashank.koirala33@gmail.com>
2023-07-30 19:04:17 +0545

    push

--- PyramidSpace.java
+++ PyramidSpace.java
@@ -1,11 +1,11 @@
 class PyramidSpace {
     public static void main(String[] args) {
         int i, j, k;
-        for (i = 1; i <= 5; i++) {
-            for (j = 5; j >= i; j--) {
+        for (i = 1; i <= 3; i++) {
+            for (j = 3; j >= i; j--) {
                 System.out.print(" ");
             }
-            for (k = 1; k <= i; k++) {
+            for (k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("* ");
             }
             System.out.print("\n");
