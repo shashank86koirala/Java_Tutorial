b61bc0083a2a49d78562f28409e9bb423515504a
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-01 09:04:10 +0545

    Dimond Pyramid

--- PyramidDiamond.java
+++ PyramidDiamond.java
@@ -1,14 +1,33 @@
-class PyramidDiamond
-{
+class PyramidDiamond {
     public static void main(String[] args) {
-        int i,j,k;
-        for(i=1; i<=5; i++)
-        {
-            for(j=1; j<=5; j++)
-            {
+        int i, j, k, l;
+        for (i = 1; i <= 5; i++) {
+            for (j = i; j < 5; j++) {
                 System.out.print(" ");
             }
-            for(k)
+            for (k = 1; k <= i; k++) {
+
+                System.out.print("*");
+            }
+            for (l = 1; l <= i; l++) {
+
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (i = 1; i <= 5; i++) {
+            for (j = 5; j < 1; j++) {
+                System.out.print(" ");
+            }
+            for (k = 5; k <= 1; k--) {
+
+                System.out.print("*");
+            }
+            for (l = 1; l <= i; l++) {
+
+                System.out.print("*");
+            }
+            System.out.print("\n");
         }
     }
 }
