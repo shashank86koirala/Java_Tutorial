35de32c7ee2f56c3ccb05bce6172088934325589
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-01 09:28:20 +0545

    push

--- PyramidDiamond.java
+++ PyramidDiamond.java
@@ -1,29 +1,21 @@
 class PyramidDiamond {
     public static void main(String[] args) {
-        int i, j, k, l;
+        int i, j, k;
         for (i = 1; i <= 5; i++) {
             for (j = i; j < 5; j++) {
                 System.out.print(" ");
             }
-            for (k = 1; k <= i; k++) {
-
-                System.out.print("*");
-            }
-            for (l = 1; l <= i; l++) {
+            for (k = 1; k <= (2 * i - 1); k++) {
 
                 System.out.print("*");
             }
             System.out.print("\n");
         }
-        for (i = 1; i <= 5; i++) {
-            for (j = 5; j < 1; j++) {
+        for (i = 4; i >= 1; i--) {
+            for (j = 5; j > i; j--) {
                 System.out.print(" ");
             }
-            for (k = 5; k <= 1; k--) {
-
-                System.out.print("*");
-            }
-            for (l = 1; l <= i; l++) {
+            for (k = 1; k <= (2 * i - 1); k++) {
 
                 System.out.print("*");
             }
