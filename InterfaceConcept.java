702d94ee92b9c1a83c896b0cee9015006fed1e38
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-05 23:30:10 +0545

    push

---
+++ InterfaceConcept.java
@@ -0,0 +1,27 @@
+interface Clientss {
+    void design();
+
+    void implementation();
+}
+
+abstract class Designer implements Clientss {
+    public void design() {
+        System.out.println("your deign table, form, color, size, demand");
+    }
+}
+
+class Developers extends Designer {
+    public void implementation() {
+        System.out.println("coding, html, css, javascript");
+    }
+
+}
+
+class timi {
+
+    public static void main(String[] args) {
+        Developers sc = new Developers();
+        sc.design();
+        sc.implementation();
+    }
+}
