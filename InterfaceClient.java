4dcd9ddd65a866cba2d3246f25b64fa79e236f25
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-05 22:41:26 +0545

    push

--- InterfaceClient.java
+++ InterfaceClient.java
@@ -1,4 +1,30 @@
-interface interfaceClient
-{
+import java.util.Scanner;
 
+interface Client {
+    void input();// public+abstract
+
+    void output();// public+abstract
+}
+
+class InterfaceClient implements Client {
+    String name;
+    double salary;
+
+    public static void main(String[] args) {
+        InterfaceClient ref = new InterfaceClient();
+        ref.input();
+        ref.output();
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        name = sc.nextLine();
+        System.out.println("Enter your Salary:");
+        salary = sc.nextDouble();
+    }
+
+    public void output() {
+        System.out.println("Your Name: " + name + " Your Salary: " + salary);
+    }
 }
