547f1634bb1a65438f663495fa79cf2f8e1849a3
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-20 21:30:50 +0545

    push this thime

--- Interface1.9V.java
+++ Interface1.9V.java
@@ -1,18 +1,25 @@
-interface A
-{
-    private void add(int x, int y)
-    {
-        System.out.print("Sum of Numbers: "+ (x+y));
+interface A {
+
+    public static void call() {
+        add(20, 30);
+    }
+
+    private static void add(int x, int y) {
+        System.out.print("Sum of Numbers: " + (x + y));
     }
 }
-class B implementation A 
-{
-    public void sub(int x, int y)
-    {
-        System.out.println("Sub of Numbers: "+ (x-y));
+
+class B implements A {
+    public void sub(int x, int y) {
+        System.out.println("Sub of Numbers: " + (x - y));
     }
 }
-class Test
-{
-    public static void main(String)
+
+class Test {
+    public static void main(String[] args) {
+        B ref = new B();
+        ref.call();
+        ref.sub(30, 20);
+
+    }
 }
