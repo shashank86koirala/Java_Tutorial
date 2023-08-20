6190b7a5b134f1668ddfc80ffce3bb26db73d8f2
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-20 21:17:22 +0545

    push so long

---
+++ Interface1.9V.java
@@ -0,0 +1,18 @@
+interface A
+{
+    private void add(int x, int y)
+    {
+        System.out.print("Sum of Numbers: "+ (x+y));
+    }
+}
+class B implementation A 
+{
+    public void sub(int x, int y)
+    {
+        System.out.println("Sub of Numbers: "+ (x-y));
+    }
+}
+class Test
+{
+    public static void main(String)
+}
