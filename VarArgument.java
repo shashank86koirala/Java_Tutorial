09b2c2fe3a4ad3c3350f870e734c1e052295425e
shashank86koirala <shashank.koirala33@gmail.com>
2023-08-21 21:44:59 +0545

    push

---
+++ VarArgument.java
@@ -0,0 +1,20 @@
+class A {
+    void add(int... a) // var-args method
+    {
+        int sum = 0;
+        for (int x : a) // for each loop
+            sum = sum + x;
+        System.out.println("sum of numbers" + sum);
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        A r = new A();
+        r.add();
+        r.add(10);
+        r.add(10, 20);
+        r.add(10, 20, 30);
+        r.add(10, 20, 30, 40, 50);
+    }
+}
